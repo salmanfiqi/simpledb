07f3ca047e4afb891fc5ad658aa9f327fe5aae75
salmanfiqi <Itzsalman0801@gmail.com>
2025-06-16 18:47:44 -0500

    add meta command functions

--- src/main.c
+++ src/main.c
@@ -31,6 +31,39 @@ InputBuffer* new_input_buffer() {
   return input_buffer;
 }
 
+MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
+  if (strcmp(input_buffer->buffer, ".exit") == 0) {
+    exit(EXIT_SUCCESS);
+  } else {
+    return META_COMMAND_UNRECOGNIZED_COMMAND;
+  }
+}
+
+PrepareResult prepare_statement(InputBuffer* input_buffer,
+  Statement* statement) {
+if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+statement->type = STATEMENT_INSERT;
+return PREPARE_SUCCESS;
+}
+if (strcmp(input_buffer->buffer, "select") == 0) {
+statement->type = STATEMENT_SELECT;
+return PREPARE_SUCCESS;
+}
+
+return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void execute_statement(Statement* statement) {
+  switch (statement->type) {
+    case (STATEMENT_INSERT):
+      printf("This is where we would do an insert.\n");
+      break;
+    case (STATEMENT_SELECT):
+      printf("This is where we would do a select.\n");
+      break;
+  }
+}
+
 void print_prompt() { printf("db > "); }
 
 void read_input(InputBuffer* input_buffer) {
